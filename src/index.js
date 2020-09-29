8b8bb2fe77b3a6749823c07fa4a1582cc88d885e
atwood-bit <artemzinko@mail.ru>
2020-09-29 18:53:32 +0300

    feat: add min,max,avg functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
-
 exports.min = function min (array) {
+  if (array === undefined || array.length === 0)
   return 0;
+  let min = array[0];
+  array.forEach(function(item, i, array){
+    if (item < min) min = item;
+  });
+  return min;
 }
 
 exports.max = function max (array) {
+  if (array === undefined || array.length === 0)
   return 0;
+  let max = array[0];
+  array.forEach(function(item, i, array){
+    if (item > max) max = item;
+  });
+  return max;
 }
 
 exports.avg = function avg (array) {
+  if (array === undefined || array.length === 0)
   return 0;
+  let avg = 0;
+  array.forEach(function(item, i, array){
+    avg += item;
+  });
+  avg = avg / array.length;
+  return avg;
 }
